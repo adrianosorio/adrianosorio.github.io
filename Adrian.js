23ebd40025884ecdca2e79388434982f9746dc7c
OITAO <adrianosorio101@gmail.com>
2015-07-14 10:57:54 -0700

    This is adding in a feature to the Homepage where I can click on a mobile screen or haver while also adding media queries

--- Adrian.js
+++ Adrian.js
@@ -4,6 +4,22 @@ $(document).ready(function(){
     $('li').removeClass('active')//removes active class from all li elements
     $(this).toggleClass('active')//adds and allows for toggle of li active to class to a clicked element
   });
+  //Create a function that will allow for this to apply in only one case in lieu of every element with this class
+  
+  function socialDropDown($socialTab, $socialContent, $socialTitle){
+    $socialTab.on("click",function(){//on click
+      $socialTab.toggleClass("social-js-1");//add the class 
+      $socialContent.toggleClass("content-js-1");//add the class
+      $socialTitle.toggleClass("title-js-1");//add the class
+    
+    });
+  }
+    //function ends
+    socialDropDown( $('.social-tab-1'),$('.social-content-one'),$('.social-title-one'));
+    socialDropDown($('.social-tab-2'), $(".social-content-two"),$("social-title-two"));
+    socialDropDown($('.social-tab-3'), $(".social-content-three"),$("social-title-three"));
+    socialDropDown($('.social-tab-4'), $(".social-content-four"),$("social-title-four"));
+    
   //This will run code every second 
   setInterval(main, 1000);
  
@@ -12,7 +28,7 @@ function main(){//this is the function that is running every second
       if(slide.attr('class') == "active"){//checks if slide is active
         id.fadeIn().animate({bottom:'250'});//does animation
        }
-      else{
+      else {
        id.fadeOut().animate({bottom:'10'});//if slide is not active it resets
       }
     }
